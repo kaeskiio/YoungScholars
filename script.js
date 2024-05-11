d7953a83180faa6dd2fa82664376de995a455850
Kacem Ettahali <kacemettahali@Kacems-MBP.attlocal.net>
2024-05-10 23:46:11 -0500

    Got the button to work

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
+// Function to redirect to a new page
 function redirectToNewPage() {
-    window.location.href = 'newpage.html'; // Change 'newpage.html' to the desired URL
+    window.location.href = 'info.html'; // Change 'newpage.html' to the desired URL
 }
 
 // Add event listener to the button
-document.getElementById("Top-Left Button").addEventListener("click", function(){
-    window.location.href="info.html"
-});
+document.getElementById('myButton').addEventListener('click', redirectToNewPage);
